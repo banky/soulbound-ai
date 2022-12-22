bd80599fea47fc21d08d3f692bd924ed5af004d6
banky <bankiadebajo1@gmail.com>
2022-12-22 14:55:00 -0500

    Add test logic to mint and burn SBT

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -3,7 +3,14 @@ import type { AppProps } from "next/app";
 import "@rainbow-me/rainbowkit/styles.css";
 import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { configureChains, createClient, WagmiConfig } from "wagmi";
-import { mainnet, polygon, optimism, arbitrum } from "wagmi/chains";
+import {
+  mainnet,
+  polygon,
+  optimism,
+  arbitrum,
+  localhost,
+  hardhat,
+} from "wagmi/chains";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 import { Inconsolata } from "@next/font/google";
@@ -11,7 +18,7 @@ import { Inconsolata } from "@next/font/google";
 const inconsolata = Inconsolata({ subsets: ["latin"] });
 
 const { chains, provider } = configureChains(
-  [mainnet, polygon, optimism, arbitrum],
+  [mainnet, polygon, optimism, arbitrum, hardhat],
   [publicProvider()]
 );
 const { connectors } = getDefaultWallets({
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,5 +1,13 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { ethers } from "ethers";
 import dynamic from "next/dynamic";
+import {
+  useAccount,
+  useContractRead,
+  useContractWrite,
+  usePrepareContractWrite,
+} from "wagmi";
+import SoulboundAI from "../../out/SoulboundAI.sol/SoulboundAI.json";
 
 // This is needed to prevent hydration issues since the server doesn't know when an account is connected
 export const MnemonicNoSSR = dynamic(
@@ -10,6 +18,38 @@ export const MnemonicNoSSR = dynamic(
 );
 
 export default function Home() {
+  const contractAddress = "0x5fc8d32690cc91d4c39d9d3abcbd16989f875707";
+  const { address } = useAccount();
+  const { data: balance } = useContractRead({
+    address: contractAddress,
+    abi: SoulboundAI.abi,
+    functionName: "balanceOf",
+    args: [address],
+  });
+
+  const { config: mintConfig } = usePrepareContractWrite({
+    address: contractAddress,
+    abi: SoulboundAI.abi,
+    functionName: "safeMint",
+    args: [address],
+    overrides: {
+      value: ethers.utils.parseEther("0.01"),
+    },
+  });
+  const {
+    data,
+    isLoading,
+    isSuccess,
+    write: mint,
+  } = useContractWrite(mintConfig);
+
+  const { config: burnConfig } = usePrepareContractWrite({
+    address: contractAddress,
+    abi: SoulboundAI.abi,
+    functionName: "burn",
+  });
+  const { write: burn } = useContractWrite(burnConfig);
+
   return (
     <>
       <header className="flex justify-between">
@@ -25,8 +65,18 @@ export default function Home() {
           <MnemonicNoSSR />
         </div>
 
-        <button className="bg-white rounded-sm px-12 py-2 text-blue mx-auto block w-fit">
+        <button
+          onClick={() => mint?.()}
+          className="bg-white rounded-sm px-12 py-2 text-blue mx-auto block w-fit"
+        >
           Mint
+        </button>
+
+        <button
+          onClick={() => burn?.()}
+          className="bg-white rounded-sm px-12 py-2 text-blue mx-auto block w-fit"
+        >
+          Burn
         </button>
       </main>
     </>
