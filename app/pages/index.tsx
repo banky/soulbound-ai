36583ea79b72bb64d67045c1b7aa593bd91c5393
banky <bankiadebajo1@gmail.com>
2022-12-22 12:39:08 -0500

    Add rainbowkit and dependencies

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,5 +1,11 @@
-import { Inter } from "@next/font/google";
+import { Inconsolata } from "@next/font/google";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <ConnectButton />
+    </div>
+  );
 }
