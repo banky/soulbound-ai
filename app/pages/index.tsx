7d1a7db094c750e38eca29f89cd8380336463b8d
Banky Adebajo <bankiadebajo1@gmail.com>
2022-12-23 06:41:01 -0500

    Configure CI (#1)
    * Working directory fix
    * Restructure project a bit

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,7 +1,7 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { useAccount } from "wagmi";
-import { MintButton } from "./components/mint-button";
-import { Mnemonic } from "./components/mnemonic";
+import { MintButton } from "../components/mint-button";
+import { Mnemonic } from "../components/mnemonic";
 
 export default function Home() {
   const { address } = useAccount();
