6c4cb433adc7bc355779722d7e46d0acc3220d0d
banky <bankiadebajo1@gmail.com>
2023-01-05 17:47:23 -0500

    Add default hardhat private key

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -16,7 +16,11 @@ const config: HardhatUserConfig = {
     },
     goerli: {
       url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-      accounts: [process.env.GOERLI_PRIVATE_KEY ?? ""],
+      // Default to well known hardhat private key
+      accounts: [
+        process.env.GOERLI_PRIVATE_KEY ??
+          "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
+      ],
     },
   },
 };
