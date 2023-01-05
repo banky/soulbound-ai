137578281d0d4ccbeb94390e08016e537beb1e55
banky <bankiadebajo1@gmail.com>
2023-01-05 17:38:55 -0500

    Add ci steps for DB migration

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,5 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
@@ -11,6 +13,10 @@ const config: HardhatUserConfig = {
       mining: {
         auto: true,
       },
+    },
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [process.env.GOERLI_PRIVATE_KEY ?? ""],
     },
   },
 };
