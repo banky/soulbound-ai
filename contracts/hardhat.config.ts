d618c807d5afbd852dc9627c47090b057a5aff26
Banky Adebajo <bankiadebajo1@gmail.com>
2023-01-06 10:40:15 -0500

    Set up goerli branch (#2)
    * Add ci steps for DB migration
    
    * Add default hardhat private key
    
    * Try to fix CI by regenerating package.lock
    
    * Npm install force
    
    * Add run for npm migrate
    
    * Add environment to app ci
    
    * Try printing env vars
    
    * Hardcode goerli as environment
    
    * Remove gitlab ci changes for environments
    
    * Add db migrate step to vercel build

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,5 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
@@ -11,6 +13,14 @@ const config: HardhatUserConfig = {
       mining: {
         auto: true,
       },
+    },
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      // Default to well known hardhat private key
+      accounts: [
+        process.env.GOERLI_PRIVATE_KEY ??
+          "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
+      ],
     },
   },
 };
