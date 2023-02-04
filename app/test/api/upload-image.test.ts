52b09554cd77b70eda620d8755f2ff27750997a5
banky <bankiadebajo1@gmail.com>
2023-02-04 14:19:51 -0800

    Add check for max number of files uploaded

--- app/test/api/upload-image.test.ts
+++ app/test/api/upload-image.test.ts
@@ -4,12 +4,25 @@ import { NextApiRequest, NextApiResponse } from "next";
 import * as NextAuth from "next-auth";
 import * as ContractReads from "helpers/contract-reads";
 import handler from "pages/api/upload-image";
+import prisma from "clients/__mocks__/prisma";
+import { ImageModel } from "@prisma/client";
+import { MAX_FILES } from "constant/image-upload";
 
 vi.mock("clients/prisma");
 vi.mock("next-auth");
 vi.mock("helpers/contract-reads");
 
 const mockAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
+const imageModel: ImageModel = {
+  batchId: "mock-batch-id",
+  modelId: "mock-model-id",
+  owner: mockAddress,
+  s3Urls: [],
+  state: "NEEDS_IMAGES",
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  descriptor: null,
+};
 
 describe("/api/upload-image", () => {
   describe("POST", () => {
@@ -58,6 +71,22 @@ describe("/api/upload-image", () => {
       });
     });
 
+    it("does nothing if we have uploaded enough files", async () => {
+      vi.spyOn(NextAuth, "getServerSession").mockResolvedValue({
+        address: mockAddress,
+      });
+      vi.spyOn(ContractReads, "addressHasSBT").mockResolvedValue(true);
+      prisma.imageModel.findUnique.mockResolvedValue({
+        ...imageModel,
+        s3Urls: Array(MAX_FILES).fill("mock-s3-url"),
+      });
+
+      await handler(req, res);
+
+      expect(prisma.imageModel.update).not.toHaveBeenCalled();
+      expect(res._getStatusCode()).toBe(200);
+      expect(res._getJSONData()).toEqual({});
+    });
     /**
      * Not sure how to do tests for the form file uploads
      */
