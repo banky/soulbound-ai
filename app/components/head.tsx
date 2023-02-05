0c59eab86672dfc0e7306f0aeaccbeecd28a9c52
banky <bankiadebajo1@gmail.com>
2023-02-05 10:08:15 -0800

    Use absolute URLs for social media share image

--- app/components/head.tsx
+++ app/components/head.tsx
@@ -14,21 +14,24 @@ export const Head = () => {
         property="og:description"
         content="Mint a unique SoulBound NFT using AI"
       />
-      <meta property="og:image" content="/preview-image.png" />
+      <meta
+        property="og:image"
+        content="https://soulbound-ai.com/preview-image.png"
+      />
 
       {/* <!-- Twitter Meta Tags --> */}
       <meta name="twitter:card" content="summary_large_image" />
-      <meta
-        property="twitter:domain"
-        content="soulbound-ai-goerli.vercel.app"
-      />
+      <meta property="twitter:domain" content="soulbound-ai.com" />
       <meta property="twitter:url" content="/" />
       <meta name="twitter:title" content="Soulbound AI" />
       <meta
         name="twitter:description"
         content="Mint a unique SoulBound NFT using AI"
       />
-      <meta name="twitter:image" content="/preview-image.png" />
+      <meta
+        name="twitter:image"
+        content="https://soulbound-ai.com/preview-image.png"
+      />
     </NextHead>
   );
 };
