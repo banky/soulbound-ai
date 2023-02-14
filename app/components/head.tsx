7f4076da941a277483b36a8100d184c4d51a2ad3
banky <bankiadebajo1@gmail.com>
2023-02-14 11:35:47 -0500

    Add favicon

--- app/components/head.tsx
+++ app/components/head.tsx
@@ -18,6 +18,25 @@ export const Head = () => {
         property="og:image"
         content="https://soulbound-ai.com/preview-image.png"
       />
+
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
     </NextHead>
   );
 };
