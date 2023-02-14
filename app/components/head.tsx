647bc2ba2d98e067ce7458468a007b791081674a
banky <bankiadebajo1@gmail.com>
2023-02-14 11:38:23 -0500

    Lowercase title

--- app/components/head.tsx
+++ app/components/head.tsx
@@ -3,13 +3,13 @@ import { default as NextHead } from "next/head";
 export const Head = () => {
   return (
     <NextHead>
-      <title>Soulbound AI</title>
+      <title>soulbound ai</title>
       <meta name="description" content="Mint a unique SoulBound NFT using AI" />
 
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content="@0xbanky" />
       <meta property="og:url" content="https://soulbound-ai.com" />
-      <meta property="og:title" content="Soulbound AI" />
+      <meta property="og:title" content="soulbound ai" />
       <meta
         property="og:description"
         content="Mint a unique SoulBound NFT using AI"
